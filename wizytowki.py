cb7df3904059c3ef7d2790a95c49307eb16b1cde
Marcin <80240893+MarcinAn@users.noreply.github.com>
2023-08-11 13:53:50 +0200

    Update wizytowki.py

--- wizytowki.py
+++ wizytowki.py
@@ -9,14 +9,14 @@ class BaseContact:
         self.surname = surname
         self.phone = phone
         self.email_address = email_address
-        self.label_len = len(f"{self.name} {self.surname}")
+        self._label_len = len(f"{self.name} {self.surname}")
 
     def contact(self):
         print(f"Wybieram numer {self.phone} i dzwonię do {self.name} {self.surname}")
 
     @property
     def label_length(self):
-        return self.label_len
+        return self._label_len
 
 
 class BusinessContact(BaseContact):
@@ -25,11 +25,11 @@ class BusinessContact(BaseContact):
         self.position = position
         self.company_name = company_name
         self.work_phone = work_phone
-        self.label_len = len(f"{self.name} {self.surname}")
 
-    @property
-    def label_length(self):
-        return self.label_len
+    def contact(self):
+        print(
+            f"Wybieram numer {self.work_phone} i dzwonię do {self.name} {self.surname}"
+        )
 
 
 def create_contacts(contact_type, number_of_contacts_to_generate):
@@ -61,60 +61,6 @@ def create_contacts(contact_type, number_of_contacts_to_generate):
 
 
 if __name__ == "__main__":
-    base_contacts = [
-        BaseContact("Bryan", "Tackett", "123-234-3456", "BryanNTackett@dayrep.com"),
-        BaseContact("Steven", "Richard", "234-345-4567", "StevenRRichard@teleworm.us"),
-        BaseContact("Bessie", "Gilbert", "345-456-5678", "BessieCGilbert@rhyta.com"),
-        BaseContact("Maryjane", "Lee", "456-567-6789", "MaryjaneJLee@armyspy.com"),
-        BaseContact("Deanna", "Smith", "567-678-7890", "DeannaDSmith@teleworm.us"),
-    ]
-    business_contacts = [
-        BusinessContact(
-            "Passenger booking clerk",
-            "Wag",
-            "201-729-4926",
-            "Irene",
-            "Richardson",
-            "111-111-1111",
-            "IreneGRichardson@jourrapide.com",
-        ),
-        BusinessContact(
-            "Pilates instructor",
-            "Jay Jacobs",
-            "208-352-0154",
-            "Theodore",
-            "Sutton",
-            "222-222-2222",
-            "TheodoreSSutton@armyspy.com",
-        ),
-        BusinessContact(
-            "Support specialist",
-            "Red Bears Tavern",
-            "812-553-2570",
-            "William",
-            "Turner",
-            "333-333-3333",
-            "WilliamNTurner@jourrapide.com",
-        ),
-        BusinessContact(
-            "Illustrator",
-            "Del Farm",
-            "773-678-5460",
-            "Donald",
-            "Albright",
-            "444-444-4444",
-            "DonaldDAlbright@teleworm.us",
-        ),
-        BusinessContact(
-            "Business management consultant",
-            "Mr. Clark's Appliances",
-            "918-384-6852",
-            "Florence",
-            "Goodwin",
-            "555-555-5555",
-            "FlorenceMGoodwin@teleworm.us",
-        ),
-    ]
 
-    generate_contact = create_contacts("base", 5)
-    generate_contact[1].contact()
+    generate_contact = create_contacts("business", 5)
+    print(generate_contact[1].label_length)
